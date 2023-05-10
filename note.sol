be224369c0233fb328ca7aad4b5f984c34650811
kiki <ejo2123@naver.com>
2023-05-10 11:30:32 +0900

    mapping final

--- note.sol
+++ note.sol
@@ -9,6 +9,21 @@ contract abc {
         string[] classes;
     }
 
+    Student[] students;
+
+    function setStudents(string memory _name, uint _number, string[] memory _classes) public {
+        students.push(Student(_name, _number, _classes));
+    }
+
+    function getStudents(uint _n) public view returns(Student memory) {
+        return students[_n - 1];
+    }
+
+    function getName(uint _n) public view returns(string memory) {
+        return students[_n - 1].name;
+    }
+
+
     mapping(string => Student[]) Teacher_Student;
 
     function pushTeacher(string memory _teacher, string memory _name, uint _number, string[] memory _classes) public {
@@ -19,5 +34,12 @@ contract abc {
         return Teacher_Student[_teacher];
     }
     // 결과 :     0:    tuple(string,uint256,string[])[]: bob,1,math,cathy,2,english
+
+
+
+    // n번 요소의 number를 수정하고 싶을 때!
+    function set(string memory _teacher, uint _n, uint _number) public {
+        Teacher_Student[_teacher][_n - 1].number = _number;
+    }
 
 }
